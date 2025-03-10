2849f25bb2f9a37c70cb1e3ce167bcc6734eaafc
Jumvi <judahmvi@gmail.com>
2025-03-10 16:43:11 +0100

    initialize project structure with basic configurations and files

---
+++ start/routes.ts
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Routes file
+|--------------------------------------------------------------------------
+|
+| The routes file is used for defining the HTTP routes.
+|
+*/
+
+import router from '@adonisjs/core/services/router'
+
+router.on('/').render('pages/home').as('home')
+
+router.on('/pages/goToUrl').render('pages/goToUrl').as('goToUrl')
